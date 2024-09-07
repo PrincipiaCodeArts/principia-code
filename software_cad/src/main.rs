5d711d4412c7d7915c8e8a4fe4d09de5d4d51e19
Artur Assis Alves <arturassiscomp@gmail.com>
2024-09-07 17:28:24 -0300

    first prototype

--- software_cad/src/main.rs
+++ software_cad/src/main.rs
@@ -1,3 +1,51 @@
+use std::{fs::File, io::Write, process::Command};
+
+struct MainFunction {
+    source_code: String,
+}
+
+const MAIN_FILE_NAME: &str = "tmp_main";
+const RUST_EXTENSION: &str = ".rs";
+const EXE_EXTENSION: &str = ".exe";
+impl MainFunction {
+    pub fn new() -> Self {
+        Self {
+            source_code: r#"
+fn main(){
+    println!("Wohoo, executing from Software CAD! Hello world!");
+}
+"#
+            .to_string(),
+        }
+    }
+
+    fn pre_compile(&self) -> &str {
+        self.source_code.as_str()
+    }
+
+    fn to_tmp_file(&self) -> Result<(), &'static str> {
+        let pre_compiled_source_code = self.pre_compile().to_string();
+        let mut file = File::create(format!("{}{}", MAIN_FILE_NAME, RUST_EXTENSION)).unwrap();
+        file.write(pre_compiled_source_code.as_bytes()).unwrap();
+
+        Ok(())
+    }
+
+    pub fn run(&self) {
+        let _ = self.to_tmp_file();
+        println!("Compiling...");
+        Command::new("rustc")
+            .arg(format!("{}{}", MAIN_FILE_NAME, RUST_EXTENSION))
+            .spawn()
+            .expect("Run failed!");
+        println!("Executing the program...");
+        Command::new(format!("./{}{}", MAIN_FILE_NAME, EXE_EXTENSION))
+            .spawn()
+            .expect("Compiled program run failed!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_code = MainFunction::new();
+    my_code.run();
 }
---
+++ tmp_main.rs
@@ -0,0 +1,4 @@
+
+fn main(){
+    println!("Wohoo, executing from Software CAD! Hello world!");
+}
